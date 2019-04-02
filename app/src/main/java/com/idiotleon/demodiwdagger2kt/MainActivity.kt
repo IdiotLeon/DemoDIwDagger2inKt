7af0a8924fe04aa7b164236c095db0b0f06cd5a1
Yang Lu (Leon) <yanglyu.leon.7@gmail.com>
2019-04-01 20:28:30 -0700

    tutorial: Field Injection

--- app/src/main/java/com/idiotleon/demodiwdagger2kt/MainActivity.kt
+++ app/src/main/java/com/idiotleon/demodiwdagger2kt/MainActivity.kt
@@ -4,17 +4,19 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.idiotleon.demodiwdagger2kt.car.Car
 import com.idiotleon.demodiwdagger2kt.car.DaggerCarComponent
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var car:Car
+    @Inject
+    lateinit var car: Car
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var carComponent = DaggerCarComponent.create()
-        car = carComponent.getCar()
+        carComponent.inject(this)
         car.drive()
     }
 }
--- app/src/main/java/com/idiotleon/demodiwdagger2kt/car/CarComponent.kt
+++ app/src/main/java/com/idiotleon/demodiwdagger2kt/car/CarComponent.kt
@@ -1,8 +1,13 @@
 package com.idiotleon.demodiwdagger2kt.car
 
+import com.idiotleon.demodiwdagger2kt.MainActivity
 import dagger.Component
 
 @Component
 interface CarComponent {
+    // For constructor injection
     fun getCar(): Car
+
+    // For field injectioin
+    fun inject(mainActivity: MainActivity)
 }
